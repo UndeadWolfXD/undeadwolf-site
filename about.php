bad9925b162357c6490f23cf564c32b39330db36
UndeadWolfXD <91128230+UndeadWolfXD@users.noreply.github.com>
2023-09-18 15:19:56 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,67 @@
+<?php
+include_once 'header.php'
+?>
+
+
+
+	<div class="overlay">
+		<h1>
+			<u>about me</u>
+			<br>
+			Hobbies and interests
+		</h1>
+		<h2>Am i a cat or a dog person?</h2>
+		<img src="img/happy_kitten.webp" alt=happy_kitten width="400">
+		<h2>Cats all the way baby!<br>I am partial to a good old dog tho</h2>
+		<p>I like anime, manga, rock music, drinking and computers are pretty cool as well<br>heres some of my top 3 favs of my topics are</p>
+		<details>
+		<summary>
+			Anime:
+		<span class="what the fuck am i doing">
+		</summary>
+		<p>
+			3: Neon Genesis Evangelion: The End Of Evangelion<br>
+			<img src="img/asuka.gif" alt="asuka" width="400"><br>
+			2: Made In Abyss: Dawn Of The Deep Soul<br>
+			<img src="img/bondrewd.webp" alt="bondrewd" width="400"><br>
+			1: Steins;gate<br>
+			<img src="img/okabe.gif" alt="okabe" width="400"><br>
+		</p>
+		</details>
+		<details>
+		<summary>
+			Manga:
+		<span class="what the fuck am i doing">
+		</summary>
+		<p>
+		</p>
+		</details>
+		<details>
+		<summary>
+			Bands:
+		<span class="what the fuck am i doing">
+		</summary>
+		<p>
+		</p>
+		</details>
+		<details>
+		<summary>
+			Games:
+		<span class="what the fuck am i doing">
+		</summary>
+		<p>
+		</p>
+		</details>
+		<details>
+		<summary>
+			Computer things?: idk im running out of ideas
+		<span class="what the fuck am i doing">
+		</summary>
+		<p>
+		</p>
+		</details>
+	</div>
+
+<?php
+include_once 'footer.php'
+?>
